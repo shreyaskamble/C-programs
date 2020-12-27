71138e7a13d4165a655185c4d213452d6dc3ccd9
shreyaskamble <76391224+shreyaskamble@users.noreply.github.com>
2020-12-27 19:46:54 +0530

    Add BeginnersBook.c

---
+++ BeginnersBook.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+
+void main()
+{
+    char s1[20]= "BeginnersBook";
+    char s2[20]= "BeginnersBook.com";
+    if (strcmp( s1 , s2) == 0)
+    {
+        printf("string 1 and string 2 are different");
+    }
+    else
+    {
+        printf("string 1 and string 2 are different");
+    }
+}
