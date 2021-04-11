7b504eff475459eb25300ea008704f3df0b89308
Shreyas Kamble <shreyask1111@gmail.com>
2021-04-11 19:28:31 +0530

    Added Averageof4Number.c

---
+++ Averageof4Number.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+    int avg=0;
+    int sum=0;
+    int x=0;
+
+    /* Array-declaration Length 4*/
+    int num[4];
+
+    /* We are using a for Loop to traverse through the array
+     * while storing the entered values in the array
+    */
+   for (x=0; x<4;x++)
+   {
+       printf("Enter number %d \n",(x+1));
+       scanf("%d",&num[x]);
+       sum = sum+num[x];
+   }
+   // for (x=0; x<4; x++)
+   //{
+   //   sum = sum=num[x];    
+   //}
+
+   avg = sum/4;
+   printf("Average of entered number is: %d",avg);
+   
+   return 0;
+
+}
